0c9d6323df2038442f9cf911075db69ce2dedd09
RXstoinkboy <erykludwin@gmail.com>
2019-03-11 23:49:38 +0100

    added build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5b043d0495e64c72f8490ae55baf5806.js"
+  "/precache-manifest.b4e301fc2bd88db4c7ddcb5cd18690b9.js"
 );
 
 workbox.clientsClaim();
---
+++ src/__tests__/trialtest.test.js
@@ -0,0 +1,3 @@
+test('Addition of 2 + 2 gives us 4', ()=> {
+    expect(2+2).toBe(4)
+})
